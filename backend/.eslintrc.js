489dd730b62b22351528b89a9e145988a43ddaae
Johannes Filter <hi@jfilter.de>
2017-04-21 13:25:05 +0300

    add eslint config

---
+++ backend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+      "node": true,
+      "es6": true,
+    },
+    "parser": "babel-eslint",
+};
