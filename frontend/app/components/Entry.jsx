e7d1d9911fa22fc2143f5ed8b76a7ab1bd0d83eb
Johannes Filter <hi@jfilter.de>
2017-04-23 23:39:54 +0300

    style results

--- frontend/app/components/Entry.jsx
+++ frontend/app/components/Entry.jsx
@@ -5,20 +5,27 @@ const Entry = ({ englTerm, estonianTermsAsList }) => {
   return (
     <div className="entry">
       <h2>
-        {englTerm}
+        <span className="englTerm">
+          {englTerm}
+        </span>
       </h2>
       {estonianTermsAsList.map((estonianTerm, index) => {
         let notes = null;
-        if (estonianTerm && estonianTerm.notes) {
+        if (estonianTerm.notes) {
           notes = (
-            <div>
-              <span className="glyphicon glyphicon-info-sign" />
-              <em>Notes</em>: { estonianTerm.notes }
+            <div className="row">
+              <div className="col-xs-3">
+                <span className="glyphicon glyphicon-info-sign" />
+                <em>Notes:</em>
+              </div>
+              <div className="col-xs-9">
+                { estonianTerm.notes }
+              </div>
             </div>);
         }
 
         let rules = null;
-        if (estonianTerm && estonianTerm.rule) {
+        if (estonianTerm.rule) {
           rules = estonianTerm.rule.map((rule) => {
             let cleanedNumber = rule.number;
             // Sometimes, it's necessary to remove other characters.
@@ -26,27 +33,31 @@ const Entry = ({ englTerm, estonianTermsAsList }) => {
               cleanedNumber = cleanedNumber.slice(0, cleanedNumber.length - 1);
             }
             return (
-              <div key={rule.number}>
-                <span className="glyphicon glyphicon glyphicon-list-alt" />
-                <a target="_blank" rel="noreferrer noopener" href={`http://www.eki.ee/dict/qs/muuttyybid.html#${cleanedNumber}`}>
-                  <em>Rule {rule.number}</em>
-                </a>
-                {`: ${rule.text}`}
+              <div key={rule.number} className="row">
+                <div className="col-xs-3">
+                  <span className="glyphicon glyphicon glyphicon-list-alt" />
+                  <a target="_blank" rel="noreferrer noopener" href={`http://www.eki.ee/dict/qs/muuttyybid.html#${cleanedNumber}`}>
+                    <em>Rule {rule.number}:</em>
+                  </a>
+                </div>
+                <div className="col-xs-9">
+                  {rule.text}`
+                </div>
               </div>);
           });
         }
 
         // Only show ordinals when there are more then one item.
         let ordinalNumber = '';
-        if (estonianTermsAsList.length > 1) {
+        // if (estonianTermsAsList.length > 1) {
           ordinalNumber = `${(index + 1)}. `;
-        }
+        // }
 
         return (
           <div key={estonianTerm.estTerm}>
-            <h3>
+            <h4>
               {ordinalNumber + estonianTerm.estTerm}
-            </h3>
+            </h4>
               { notes }
             <div>
               { rules }
