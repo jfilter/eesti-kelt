ef293a7510ce57c46ad5fc3dbc54eb66f919c13d
Johannes Filter <hi@jfilter.de>
2017-04-23 22:42:42 +0300

    rename variables

--- frontend/app/components/Entry.jsx
+++ frontend/app/components/Entry.jsx
@@ -1,53 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Entry = ({ englTerm, details }) => {
+const Entry = ({ englTerm, estonianTermsAsList }) => {
   return (
     <div className="entry">
       <h2>
         {englTerm}
       </h2>
-      {details.map((y, index) => {
+      {estonianTermsAsList.map((estonianTerm, index) => {
         let notes = null;
-        let rule = null;
-        if (y && y.notes) {
+        if (estonianTerm && estonianTerm.notes) {
           notes = (
             <div>
               <span className="glyphicon glyphicon-info-sign" />
-              <span />
-              { y.notes }
+              <em>Notes</em>: { estonianTerm.notes }
             </div>);
         }
 
-        if (y && y.rule) {
-          rule = y.rule.map((yy) => {
-            let pureNumber = yy.number;
-            if (pureNumber.match(/[a-z]/i)) {
-              pureNumber = pureNumber.slice(0, pureNumber.length - 1);
+        let rules = null;
+        if (estonianTerm && estonianTerm.rule) {
+          rules = estonianTerm.rule.map((rule) => {
+            let cleanedNumber = rule.number;
+            // Sometimes, it's necessary to remove other characters.
+            if (cleanedNumber.match(/[a-z]/i)) {
+              cleanedNumber = cleanedNumber.slice(0, cleanedNumber.length - 1);
             }
             return (
-              <div key={yy.number}>
+              <div key={rule.number}>
                 <span className="glyphicon glyphicon glyphicon-list-alt" />
-                <a href={`http://www.eki.ee/dict/qs/muuttyybid.html#${pureNumber}`}>
-                  {yy.number}
+                <a target="_blank" rel="noreferrer noopener" href={`http://www.eki.ee/dict/qs/muuttyybid.html#${cleanedNumber}`}>
+                  <em>Rule {rule.number}</em>
                 </a>
-                {`: ${yy.text}`}
+                {`: ${rule.text}`}
               </div>);
           });
         }
 
-        let optionalText = '';
-        if (details.length > 1) {
-          optionalText = `${(index + 1)}. `;
+        // Only show ordinals when there are more then one item.
+        let ordinalNumber = '';
+        if (estonianTermsAsList.length > 1) {
+          ordinalNumber = `${(index + 1)}. `;
         }
 
         return (
-          <div key={y.estTerm}>
+          <div key={estonianTerm.estTerm}>
             <h3>
-              {optionalText + y.estTerm}
+              {ordinalNumber + estonianTerm.estTerm}
             </h3>
-            { notes }
-            { rule }
+              { notes }
+            <div>
+              { rules }
+            </div>
           </div>
         );
       })}
@@ -57,7 +60,7 @@ const Entry = ({ englTerm, details }) => {
 
 Entry.propTypes = {
   englTerm: PropTypes.string.isRequired,
-  details: PropTypes.arrayOf(PropTypes.shape({
+  estonianTermsAsList: PropTypes.arrayOf(PropTypes.shape({
     estTerm: PropTypes.string.isRequired,
     notes: PropTypes.string,
     rules: PropTypes.arrayOf(PropTypes.shape({
